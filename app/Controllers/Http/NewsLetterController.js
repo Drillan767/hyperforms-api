deec7981a327c78826c480dbf9673cc6f7341c6d
Drillan767 <jd.levarato@gmail.com>
2019-01-01 20:43:25 +0100

    Started to work on Musics and Newsletter, but I need to sleep

--- app/Controllers/Http/NewsLetterController.js
+++ app/Controllers/Http/NewsLetterController.js
@@ -1,6 +1,17 @@
 'use strict'
 
 class NewsLetterController {
+  async getList ({response}) {}
+
+  async deleteFromList ({request, response}) {}
+
+  async saveNLSettings ({request, response}) {}
+
+  async createNL ({request, response}) {}
+
+  async publishNL ({request, response}) {}
+
+  async uploadNL ({request, response}) {}
 }
 
 module.exports = NewsLetterController
---
+++ database/migrations/1546367469736_nl_setting_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class NlSettingSchema extends Schema {
+  up () {
+    this.create('nl_settings', (table) => {
+      table.increments()
+      table.text('template')
+      table.string('bottom_text')
+      table.string('unsubscribe')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('nl_settings')
+  }
+}
+
+module.exports = NlSettingSchema
